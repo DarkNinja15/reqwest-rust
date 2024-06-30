c2116a5f4681a4293d8d567889cddcf0cbd902ec
Animesh Shukla <animeshshukla1518@gmail.com>
2024-07-01 00:42:14 +0530

    feat: made blocking get request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Read;
+
+use error_chain::error_chain;
+
+error_chain!{
+    foreign_links{
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+fn main()->Result<()> {
+    let mut resp=reqwest::blocking::get("http://httpbin.org/get")?;
+    let mut body=String::new();
+    resp.read_to_string(&mut body)?;
+
+    println!("Status code = {}",resp.status());
+
+    println!("Headers = \n{:?}",resp.headers());
+
+    println!("Body = \n{}",body);
+
+    Ok(())
 }

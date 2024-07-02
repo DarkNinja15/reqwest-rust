cbc58a807f8001b376b34958a797df99e77c0417
Animesh Shukla <animeshshukla1518@gmail.com>
2024-07-02 14:52:21 +0530

    feat: added get request using async

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Read;
+// use std::io::Read;
 
 use error_chain::error_chain;
 
@@ -9,16 +9,29 @@ error_chain!{
     }
 }
 
-fn main()->Result<()> {
-    let mut resp=reqwest::blocking::get("http://httpbin.org/get")?;
-    let mut body=String::new();
-    resp.read_to_string(&mut body)?;
+#[tokio::main]
+async fn main()->Result<()> {
+    // get request without async await
+    // let mut resp=reqwest::blocking::get("http://httpbin.org/get")?;
+    // let mut body=String::new();
+    // resp.read_to_string(&mut body)?;
+
+    // println!("Status code = {}",resp.status());
+
+    // println!("Headers = \n{:?}",resp.headers());
+
+    // println!("Body = \n{}",body);
+
+
+    // get request using async await
+
+    let resp=reqwest::get("http://httpbin.org/get").await.unwrap();
 
     println!("Status code = {}",resp.status());
 
     println!("Headers = \n{:?}",resp.headers());
 
-    println!("Body = \n{}",body);
+    println!("Body = \n{}",resp.text().await.unwrap());
 
     Ok(())
 }

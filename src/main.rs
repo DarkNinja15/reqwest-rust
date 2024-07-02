ec1a6e7fb0cb5430435407d9c4ea486787372105
Animesh Shukla <animeshshukla1518@gmail.com>
2024-07-02 15:06:08 +0530

    feat: added post request and serializations

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
+use reqwest::header::USER_AGENT;
 // use std::io::Read;
+use serde::Deserialize;
 
 use error_chain::error_chain;
 
-error_chain!{
+error_chain! {
     foreign_links{
         Io(std::io::Error);
         HttpRequest(reqwest::Error);
     }
 }
 
+#[derive(Deserialize, Debug)]
+struct User {
+    login: String,
+    id: u32,
+}
+
 #[tokio::main]
-async fn main()->Result<()> {
-    // get request without async await
+async fn main() -> Result<()> {
+    // ! get request without async await
     // let mut resp=reqwest::blocking::get("http://httpbin.org/get")?;
     // let mut body=String::new();
     // resp.read_to_string(&mut body)?;
@@ -22,16 +30,30 @@ async fn main()->Result<()> {
 
     // println!("Body = \n{}",body);
 
+    // ! get request using async await
 
-    // get request using async await
+    // let resp=reqwest::get("http://httpbin.org/get").await.unwrap();
 
-    let resp=reqwest::get("http://httpbin.org/get").await.unwrap();
+    // println!("Status code = {}",resp.status());
 
-    println!("Status code = {}",resp.status());
+    // println!("Headers = \n{:?}",resp.headers());
 
-    println!("Headers = \n{:?}",resp.headers());
+    // println!("Body = \n{}",resp.text().await.unwrap());
 
-    println!("Body = \n{}",resp.text().await.unwrap());
+    // ! api requests
+
+    let req_url = format!("https://api.github.com/repos/{owner}/{repos}/stargazers",
+        owner="rust-lang-nursery",
+        repos="rust-cookbook",
+    );
+
+    let client=reqwest::Client::new();
+
+    let resp=client.get(&req_url).header(USER_AGENT, "rust web-api-client demo").send().await?;
+
+    let users:Vec<User>=resp.json().await?;
+
+    println!("{:?}",users);
 
     Ok(())
 }
